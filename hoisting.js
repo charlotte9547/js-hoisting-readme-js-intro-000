06bf626ca2c84ad25d60a0df00fd6d140dd76445
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-03-24 02:23:38 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,24 +1,22 @@
+
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
 
   sayMy();
@@ -26,5 +24,5 @@ function sayMyName() {
   function sayMy() {
     console.log(name);
     var name = "Kristin";
-  }
+sayMy ()  }
 }

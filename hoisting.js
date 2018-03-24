7305b0f88d8666291514275fdab02b393ce98ec1
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-03-24 02:28:22 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -15,3 +15,15 @@ function crazy() {
   }
   thisIsCrazy();
 }
+
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
+
+  sayMy();
+
+  function sayMy() {
+    console.log(name);
+    var name = "Kristin";
+  }
+}
